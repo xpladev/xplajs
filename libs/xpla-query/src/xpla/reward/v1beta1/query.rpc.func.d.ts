0cbfa8e1d791394b9e899fc675d0954afda739e3
JoowonYun <joowon@delightlabs.io>
2024-12-24 18:50:55 +0900

    chore: remove xpla-query

--- libs/xpla-query/src/xpla/reward/v1beta1/query.rpc.func.d.ts
+++
@@ -1,6 +0,0 @@
-import { RpcResolver } from "../../../helper-func-types";
-import { QueryParamsRequest, QueryParamsResponse, QueryPoolRequest, QueryPoolResponse } from "./query";
-export declare const createGetParams: (clientResolver?: RpcResolver) => (request: QueryParamsRequest) => Promise<QueryParamsResponse>;
-export declare const useGetParams: <TData = QueryParamsResponse>({ request, options, clientResolver, customizedQueryKey, }: import("../../../react-query").UseQueryParams<QueryParamsRequest, QueryParamsResponse, TData>) => import("@tanstack/react-query").UseQueryResult<TData, Error>;
-export declare const createGetPool: (clientResolver?: RpcResolver) => (request: QueryPoolRequest) => Promise<QueryPoolResponse>;
-export declare const useGetPool: <TData = QueryPoolResponse>({ request, options, clientResolver, customizedQueryKey, }: import("../../../react-query").UseQueryParams<QueryPoolRequest, QueryPoolResponse, TData>) => import("@tanstack/react-query").UseQueryResult<TData, Error>;
--- libs/xpla-query/src/xpla/volunteer/v1beta1/query.rpc.func.d.ts
+++
@@ -1,4 +0,0 @@
-import { RpcResolver } from "../../../helper-func-types";
-import { QueryVolunteerValidatorsRequest, QueryVolunteerValidatorsResponse } from "./query";
-export declare const createGetVolunteerValidators: (clientResolver?: RpcResolver) => (request: QueryVolunteerValidatorsRequest) => Promise<QueryVolunteerValidatorsResponse>;
-export declare const useGetVolunteerValidators: <TData = QueryVolunteerValidatorsResponse>({ request, options, clientResolver, customizedQueryKey, }: import("../../../react-query").UseQueryParams<QueryVolunteerValidatorsRequest, QueryVolunteerValidatorsResponse, TData>) => import("@tanstack/react-query").UseQueryResult<TData, Error>;
