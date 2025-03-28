7d4aa06caacfa910784c1435bb8aecff86e79f39
JoowonYun <joowon@delightlabs.io>
2025-03-28 16:01:58 +0900

    feat: define precompile contract

---
+++ networks/xpla/src/evm/index.ts
@@ -0,0 +1,10 @@
+export { 
+    PrecompileBank, 
+    PrecompileBech32, 
+    PrecompileDistribution, 
+    PrecompileEvidence, 
+    PrecompileGov, 
+    PrecompileIcs20, 
+    PrecompileSlashing, 
+    PrecompileStaking 
+} from "./contracts";
