8a71a4a0d8db5e9ebdcb18d5d36f2a6196536daf
JoowonYun <joowon@delightlabs.io>
2025-08-12 15:19:53 +0900

    feat: solidity pacakge @xpla/contracts

---
+++ hardhat.config.ts
@@ -0,0 +1,18 @@
+import { HardhatUserConfig } from "hardhat/config";
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.27",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  paths: {
+    sources: "contracts",
+  },
+};
+
+export default config;
