40236069cd67ecb9c1516e479216504fa31433e5
JoowonYun <joowon@delightlabs.io>
2025-08-26 10:38:52 +0900

    fix: lcd & rpc codegen script

--- scripts/codegen.ts
+++ scripts/codegen.ts
@@ -105,16 +105,69 @@ rimraf(join(__dirname, '../libs/cosmos-types/src'));
 rimraf(join(__dirname, '../libs/xpla-react/src'));
 rimraf(join(__dirname, '../libs/xplajs/src'));
 
+const useModules = [
+  "cosmos.auth.v1beta1",
+  "cosmos.authz.v1beta1",
+  "cosmos.bank.v1beta1",
+  "cosmos.base.node.v1beta1",
+  "cosmos.base.tendermint.v1beta1",
+  "cosmos.consensus.v1",
+  "cosmos.distribution.v1beta1",
+  "cosmos.evidence.v1beta1",
+  "cosmos.evm.feemarket.v1",
+  "cosmos.evm.vm.v1",
+  "cosmos.feegrant.v1beta1",
+  "cosmos.gov.v1",
+  "cosmos.gov.v1beta1",
+  "cosmos.mint.v1beta1",
+  "cosmos.slashing.v1beta1",
+  "cosmos.staking.v1beta1",
+  "cosmos.tx.v1beta1",
+  "cosmos.upgrade.v1beta1",
+  "cosmwasm.wasm.v1",
+  "ibc.application.interchain_accounts.controller.v1",
+  "ibc.applications.interchain_accounts.host.v1",
+  "ibc.applications.interchain_accounts.transfer.v1",
+  "ibc.applications.transfer.v1",
+  "ibc.core.channel.v1",
+  "ibc.core.channel.v2",
+  "ibc.core.client.v1",
+  "ibc.core.client.v2",
+  "ibc.core.connection.v1",
+  "ibc.core.connection.v2",
+  "ibc.core.port.v1",
+  "ibc.lightclients.cosmos.v1",
+  "ibc.lightclients.evm.v1",
+  "xpla.burn.v1beta1",
+  "xpla.reward.v1beta1",
+  "xpla.volunteer.v1beta1",
+  "xpla.reward.v1beta1",
+]
+
 // xplajs
 telescope({
   protoDirs: [join(__dirname, '../protos/xplajs')],
   outPath: join(__dirname, '../libs/xplajs/src'),
   options: deepmerge(options.options, {
     "lcdClients": {
-      "enabled": true
+      "enabled": true,
+      "scoped": [
+        {
+          "dir": "xpla",
+          packages: useModules,
+          addToBundle:true,
+        }
+      ]
     },
     "rpcClients": {
-      "enabled": true
+      "enabled": true,
+      "scoped": [
+        {
+          "dir": "xpla",
+          packages: useModules,
+          addToBundle:true,
+        }
+      ]
     },
   })
 })
