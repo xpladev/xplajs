a87f46d55a546484745121054a81bd152fe9f357
Wonhee Lee <2wheeh@gmail.com>
2026-01-16 01:45:25 +0900

    fixup

--- libs/xplajs/tsdown.config.ts
+++ libs/xplajs/tsdown.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'tsdown';
 
 export default defineConfig({
-  entry: ['src/index.ts'],
+  entry: ['src/**/*.ts'],
   format: ['cjs', 'esm'],
   dts: true,
   unbundle: true,
-  treeshake: true,
   clean: true,
   outExtensions: ({ format }) => ({
     js: format === 'cjs' ? '.cjs' : '.js',
